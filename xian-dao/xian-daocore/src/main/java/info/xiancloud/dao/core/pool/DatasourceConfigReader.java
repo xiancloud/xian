9725502ecd367c41946e23f079c384c8c24c6a32
yangyuan <yangyuan@cedarhd.com>
2019-03-16 16:15:07 +0800

    优化数据源全局配置的常量名称定义

--- xian-dao/xian-daocore/src/main/java/info/xiancloud/dao/core/pool/DatasourceConfigReader.java
+++ xian-dao/xian-daocore/src/main/java/info/xiancloud/dao/core/pool/DatasourceConfigReader.java
@@ -24,27 +24,64 @@ import info.xiancloud.core.util.LOG;
 public class DatasourceConfigReader {
 
     /**
+     * configuration key of db url for writing and reading
+     */
+    public static final String DB_URL_KEY = "db_url";
+    /**
+     * configuration key of db user name for writing and reading
+     */
+    public static final String DB_USER_KEY = "db_user";
+    /**
+     * configuration key of db password for writing and reading
+     */
+    public static final String DB_PWD_KEY = "db_pwd";
+    /**
+     * configuration key of db pool size for writing and reading
+     */
+    public static final String DB_POOL_SIZE = "db_pool_size";
+    /**
+     * configuration key of db url for reading only
+     */
+    public static final String READONLY_DB_URL = "readonly_db_url";
+    /**
+     * configuration key of db user name for reading only
+     */
+    public static final String READONLY_DB_USER = "readonly_db_user";
+    /**
+     * configuration key of db password for reading only
+     */
+    public static final String READONLY_DB_PWD = "readonly_db_pwd";
+    /**
+     * configuration key of db pool size for reading only
+     */
+    public static final String READONLY_DB_POOL_SIZE = "readonly_db_pool_size";
+    /**
+     * configuration key of db transaction timeout
+     */
+    public static final String DB_TRANSACTION_TIMEOUT = "db_transaction_timeout";
+
+    /**
      * read master datasource configuration from xian config.
      * please refer to {@link XianDataSource#url}  to see database configuration format
      *
      * @return datasource configuration url
      */
     public static String getWriteUrl() {
-        String writeUrl = XianConfig.get("db_url");
+        String writeUrl = XianConfig.get(DB_URL_KEY);
         LOG.info("db_url = " + writeUrl);
         return writeUrl;
     }
 
     public static String getWriteUser() {
-        return XianConfig.get("db_user");
+        return XianConfig.get(DB_USER_KEY);
     }
 
     public static String getWritePwd() {
-        return XianConfig.get("db_pwd");
+        return XianConfig.get(DB_PWD_KEY);
     }
 
     public static int getWritePoolSize() {
-        return XianConfig.getIntValue("db_pool_size");
+        return XianConfig.getIntValue(DB_POOL_SIZE);
     }
 
     /**
@@ -54,21 +91,21 @@ public class DatasourceConfigReader {
      * @return datasource configuration url
      */
     public static String getReadUrl() {
-        String readUrl = XianConfig.get("readonly_db_url");
+        String readUrl = XianConfig.get(READONLY_DB_URL);
         LOG.info("readonly_db_url = " + readUrl);
         return readUrl;
     }
 
     public static String getReadUser() {
-        return XianConfig.get("readonly_db_user");
+        return XianConfig.get(READONLY_DB_USER);
     }
 
     public static String getReadPwd() {
-        return XianConfig.get("readonly_db_pwd");
+        return XianConfig.get(READONLY_DB_PWD);
     }
 
     public static int getReadPoolSize() {
-        return XianConfig.getIntValue("readonly_db_pool_size");
+        return XianConfig.getIntValue(READONLY_DB_POOL_SIZE);
     }
 
     /**
@@ -78,7 +115,7 @@ public class DatasourceConfigReader {
      * @return global transaction timeout in milliseconds.
      */
     public static long getTransactionTimeout() {
-        return XianConfig.getLongValue("db_transaction_timeout", 1000 * 60 * 5);
+        return XianConfig.getLongValue(DB_TRANSACTION_TIMEOUT, 1000 * 60 * 5);
     }
 
 }
